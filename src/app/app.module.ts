c28857fa658860391bc50ae8efd3643368fb6aff
Husen Telwala <husen.telwala@autofacets.com>
2020-07-27 14:38:59 +0530

    Two components and PPT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AboutUsComponent]
 })
 export class AppModule { }
